3438d7200a9f310750461eb1ef26f439ca162f8f
Mirko Grünheid <kontakt@gruenheid.net>
2026-07-03 08:39:05 +0200

    Release version 1.0.0

--- js/main.js
+++
@@ -1,7 +0,0 @@
-const user = "kontakt";
-const domain = "gruenheid.net";
-
-const email = `${user}@${domain}`;
-
-document.getElementById("email").textContent = email;
-document.getElementById("emailLink").href = `mailto:${email}`;
